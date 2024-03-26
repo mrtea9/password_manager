a61674481692431c709ee6d9df28408dfd85f131
mrtea9 <ionmeriacri111@gmail.com>
2024-03-26 16:37:09 +0200

    Password Manager GUI

--- main.py
+++ main.py
@@ -80,7 +80,6 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
